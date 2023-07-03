e916e7bfe9da3882635def68058862d8451ac412
“Enter <jadhavmahesh529@gmail.com>
2023-07-03 13:08:12 +0530

    [Mahesh] Create table code is added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,6 @@
 --UC1-Create database
 create database AddressBookService
+--UC2- Create Table
+create table AddressBook(Firstname varchar(200),lastname varchar(200),address varchar(200),city varchar(200),state varchar(200),
+zip int ,phonenumber bigint, email varchar(200));
 
